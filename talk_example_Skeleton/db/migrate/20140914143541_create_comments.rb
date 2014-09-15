cf2ed7643725e573de9c0173f48894b376b5e31e
n-zeplo <nzeplowitz@gmail.com>
2014-09-15 11:41:20 -0400

    Add Notes

--- talk_example_Skeleton/db/migrate/20140914143541_create_comments.rb
+++ talk_example_Skeleton/db/migrate/20140914143541_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.string :content
-      t.references :user
       t.references :post
+      t.references :user #=> The two below are the same thing
       # t.belongs_to :user
       # t.integer :user_id
 
